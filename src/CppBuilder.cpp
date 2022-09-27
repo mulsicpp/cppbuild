97f649afed2367e3d11f4bd1d8169c1f13dd24d1
Christian Mulser <christianmulser000@gmail.com>
2022-09-27 23:27:05 +0200

    added win32 info

--- src/CppBuilder.cpp
+++ src/CppBuilder.cpp
@@ -70,12 +70,50 @@ CppBuilder::CppBuilder(int argc, char *argv[]) : run(false), force(false)
 void CppBuilder::setup()
 {
 #if defined(_WIN32)
+
+    std::filesystem::current_path(path_To_Exe);
+    FILE *p_File;
+    fopen_s(&p_File, "cppbuild_win32_info.txt", "w");
     char buffer[MAX_PATH];
     printf("MSVC path: ");
     fgets(buffer, MAX_PATH, stdin);
+    buffer[strlen(buffer) - 1] = 0;
+    auto msvc_path = std::filesystem::path(buffer);
 
     printf("Windows Kits path: ");
     fgets(buffer, MAX_PATH, stdin);
+    buffer[strlen(buffer) - 1] = 0;
+    auto win_kits_path = std::filesystem::path(buffer);
+
+    fputs((std::string("/I\"") + (msvc_path / "include").string() + "\" ").c_str(), p_File);
+    fputs((std::string("/I\"") + (msvc_path / "atlmfc/include").string() + "\" ").c_str(), p_File);
+
+    for (const auto &subfolder : std::filesystem::directory_iterator(win_kits_path / "Include"))
+        for (const auto &entry : std::filesystem::directory_iterator(subfolder.path()))
+            fputs((std::string("/I\"") + entry.path().string() + "\" ").c_str(), p_File);
+    fprintf(p_File, "\n");
+
+    fputs((std::string("/libpath:\"") + (msvc_path / "lib/x86").string() + "\" ").c_str(), p_File);
+    fputs((std::string("/libpath:\"") + (msvc_path / "atlmfc/lib/x86").string() + "\" ").c_str(), p_File);
+
+    for (const auto &subfolder : std::filesystem::directory_iterator(win_kits_path / "Lib"))
+    {
+        fputs((std::string("/libpath:\"") + subfolder.path().string() + "/um/x86\" ").c_str(), p_File);
+        fputs((std::string("/libpath:\"") + subfolder.path().string() + "/ucrt/x86\" ").c_str(), p_File);
+    }
+    fprintf(p_File, "\n");
+
+    fputs((std::string("/libpath:\"") + (msvc_path / "lib/x64").string() + "\" ").c_str(), p_File);
+    fputs((std::string("/libpath:\"") + (msvc_path / "atlmfc/lib/x64").string() + "\" ").c_str(), p_File);
+
+    for (const auto &subfolder : std::filesystem::directory_iterator(win_kits_path / "Lib"))
+    {
+        fputs((std::string("/libpath:\"") + subfolder.path().string() + "/um/x64\" ").c_str(), p_File);
+        fputs((std::string("/libpath:\"") + subfolder.path().string() + "/ucrt/x64\" ").c_str(), p_File);
+    }
+    fprintf(p_File, "\n");
+
+    fclose(p_File);
 
     exit(0);
 #elif defined(__linux__)
--- src/CppBuilder.h
+++ src/CppBuilder.h
@@ -6,6 +6,8 @@
 struct CppBuilder {
     std::string path_To_Exe, path_To_Project;
 
+    std::string system_Include_Paths = "", system_Lib32_Paths = "", system_Lib64_Paths = "";
+
     bool run, force;
 
     Config config;
